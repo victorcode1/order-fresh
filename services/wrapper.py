af57dc88252fdb9c1bf376ab73f5f5c4db12cec1
Avery <averystampp18@gmail.com>
2023-01-08 18:15:57 -0500

    Created send order function

--- services/wrapper.py
+++ services/wrapper.py
@@ -1,4 +1,5 @@
 import requests
+from typing import TypedDict
 
 def getOrganization(token: str):
     headers = {"x-integration-token": token}
@@ -56,10 +57,93 @@ def getOrders(token: str, store: str, device: str):
     else:
         return response.status_code
 
+class Retry(TypedDict):
+    notificationUrl: str
+    expiration: str
+
+class Item(TypedDict):
+    
+    #required to create an item
+    id: str
+    name: str
+    qty: int
+    mods: list[str]
+
+    #optional fields for an item
+    lineId: str
+    price: str
+    components: list[str]
+    specialInstructions: str
 
 
+def sendOrder(
+
+#used as headers to authorize and send your order
+token: str, 
+store: str, 
+device: str, 
+
+#minimum required arguments to create an order in FreshKDS
+id: str, 
+name: str, 
+time: str,
+mode: str,
+items: list[Item],
+terminal: str,
+
+#optional arguments for an order
+phoneNumber: str = None,
+optInForSms: bool = None,
+deliveryAddress: str = None,
+server: str = None,
+source: str = None,
+pickupTime: str = None,
+specialInstructions: str = None,
+customerArrivedUrl: str = None,
+vehicleModel: str = None,
+vehicleColor: str = None,
+retry: str = None,
+costs: dict = None,
 
 
+):
 
+    if id == None or name == None or time == None or mode == None or items == None or terminal == None:
+        print("Could Not send order")
 
-        
+    headers = {
+        "x-integration-token": token,
+        "x-location-id": store,
+        "x-device-ids": device
+        }
+    url = "https://integrations-api.ftservices.cloud/integrators/kds-orders"
+
+    payload = {
+        "id": id,
+        "name": name,
+        "time": time,
+        "mode": mode,
+        "items": items,
+        "terminal": terminal,
+
+        "phoneNumber": phoneNumber,
+        "optInForSms": optInForSms,
+        "deliveryAddress": deliveryAddress,
+        "server": server,
+        "source": source,
+        "pickupTime": pickupTime,
+        "specialInstructions": specialInstructions,
+        "customerArrivedUrl": customerArrivedUrl,
+        "vehicleModel": vehicleModel,
+        "vehicleColor": vehicleColor,
+        "retry": retry,
+        "costs": costs
+    }
+
+    response = requests.request("POST", url=url, headers=headers, data=payload)
+    if response.status_code == 200:
+        return response.json()
+
+    else:
+        return response.status_code
+
