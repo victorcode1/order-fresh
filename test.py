438033ecb923bfb36be6aa03a76fe09e3ee125d4
Avery <amstampp18@gmail.com>
2023-03-22 22:44:41 -0400

    Removed tests

--- test.py
+++
@@ -1,7 +0,0 @@
-from pyKDSAPI.utils import getOrganization
-
-heads = {'Content-Type':'application/json'}
-token = "IuOjJi6uAMkEzybWjogaKYvh2J0KwVxyN7h3M3rgwAY0vlyztgB1kWo6V51V01rFh0nuirqHjutO6STCalnwzc"
-x= getOrganization(token=token)
-
-print(x)
